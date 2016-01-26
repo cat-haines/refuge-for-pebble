fcfb7d484da5874eb6d2ccdc8b0c192b9feec3e1
Cat Haines <cat@pebble.com>
2016-01-25 21:46:39 -0800

    Added new messages

--- src/refuge.h
+++ src/refuge.h
@@ -12,8 +12,14 @@
 #define OUTBOX_SIZE 128
 
 #define MSG_APP_READY 0
+#define MSG_WASHROOMS 1
+#define MSG_GETERROR  2
 
 // Text
 #define TXT_INITIALIZING "Initializing..."
 #define TXT_NO_CONNECTION "No Connection :("
 #define TXT_CONNECTED "Connected!"
+
+// Events
+#define APP_READY_EVENT 0
+#define WASHROOMS_DATA_EVENT  1
